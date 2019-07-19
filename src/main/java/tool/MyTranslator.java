a94ba265098641a28c5b2419af3d3361758f978f
zhengbangsheng <bonsonzheng@gmail.com>
2019-07-19 14:18:08 -0400

    Create MyTranslator to translate text with certain syntax

---
+++ src/main/java/tool/MyTranslator.java
@@ -0,0 +1,192 @@
+package tool;
+
+
+import java.util.*;
+
+public class MyTranslator {
+
+    public static List<String> translate(String input) {
+
+        Map<Integer, List<String>> helperMap = new HashMap<>();
+        String str = removeRedundantBrackets(input).trim();
+
+        List<String> segments = parseByBrackets(str);
+        for (int i = 0; i < segments.size(); i++) {
+            List<String> parseResult = handleSegment(segments.get(i).trim());
+            helperMap.put(i, parseResult);
+        }
+
+        return getAllCombinations(helperMap);
+    }
+
+    public static List<String> handleSegment(String input) {
+        List<String> segmentResult = new ArrayList<>();
+        if(!containsBrackets(input)) {
+            segmentResult.addAll(parseNoBracketStatement(input));
+        }else {
+            List<String> orSegments = parseByDelimeter(input,'|');
+            orSegments.forEach(seg-> segmentResult.addAll(translate(seg)));
+        }
+        return segmentResult;
+
+    }
+
+    private static String removeRedundantBrackets(String input) {
+        String s = input;
+        if(isACompletedBracketStatement(input)) {
+            s = removeLeftAndRightBrackets(input);
+        }
+
+        if(!input.contains("|")) {
+            s = removeAllBrackets(input);
+        }
+        return s;
+    }
+
+    private static List<String> parseNoBracketStatement(String input) {
+        Map<Integer, List<String>> helperMap = new HashMap<>();
+
+        String[] splits = input.split("\\s+");
+        for(int i = 0 ;i< splits.length;i++){
+            String[] tokens = splits[i].split("\\|");
+            helperMap.put(i,Arrays.asList(tokens));
+        }
+        return getAllCombinations(helperMap);
+    }
+
+    private static boolean isACompletedBracketStatement(String str) {
+        if(str.length()<2){return false;}
+        if(!(str.startsWith("{")&&str.endsWith("}"))){return false;}
+        int count = 0;
+        for(int i = 0 ; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c == '{'){
+                ++count;
+            }
+            if(c == '}'){
+                --count;
+            }
+            if(count == 0){
+                return i == str.length() - 1;
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsBrackets(String input){
+        return input.contains("{") && input.contains("}");
+    }
+
+    public static List<String> parseByBrackets(String input) {
+        Stack stack = new Stack();
+        List<String> result = new ArrayList();
+
+        StringBuffer temp = new StringBuffer("");
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            switch (c) {
+                case '{': {
+                    if (stack.isEmpty()) {
+                        if (i > 0 && input.charAt(i - 1) != '|') {
+                            String aSegment = temp.toString().trim();
+                            if (!aSegment.isEmpty()) {
+                                result.add(aSegment);
+                                temp.delete(0, temp.length());
+                            }
+                        }
+                    }
+                    temp.append(c);
+                    stack.push("{");
+                    break;
+                }
+                case '}': {
+                    temp.append(c);
+                    stack.pop();
+                    if (i < input.length() - 1 && input.charAt(i + 1) == '|') {
+                        break;
+                    } else if (stack.isEmpty()) {
+                        result.add(temp.toString().trim());
+                        temp.delete(0, temp.length());
+                    }
+                    break;
+                }
+                default: {
+                    temp.append(c);
+                }
+            }
+        }
+
+        if (!temp.toString().isEmpty()) {
+            result.add(temp.toString().trim());
+        }
+        print("parse by brackets", result);
+
+        return result;
+    }
+
+    private static List<String> parseByDelimeter(String input, char delemiter){
+        Stack stack = new Stack();
+        List<String> result = new ArrayList<>();
+        StringBuffer temp = new StringBuffer("");
+        for(int i = 0; i < input.length();i++){
+            char c = input.charAt(i);
+            if (c =='{'){
+                temp.append(c);
+                stack.push("{");
+            }else if (c=='}'){
+                temp.append(c);
+                stack.pop();
+            }else if(c == delemiter){
+                if(stack.isEmpty()){
+                    result.add(temp.toString().trim());
+                    temp.delete(0,temp.length());
+                }else{
+                    temp.append(c);
+                }
+            }else{
+                temp.append(c);
+            }
+        }
+        if (!temp.toString().isEmpty()) {
+            result.add(temp.toString().trim());
+        }
+        return result;
+    }
+
+    private static String removeLeftAndRightBrackets(String str) {
+        return str.startsWith("{") && str.endsWith("}") ? str.replaceAll("^.|.$","") : str;
+    }
+
+    private static String removeAllBrackets(String str) {
+        return str.replaceAll("\\{","").replaceAll("\\}","");
+    }
+
+    private static List<String> getAllCombinations(Map<Integer,List<String>> aMap){
+        List<String> combined = new ArrayList<>();
+
+        if(aMap.get(0)==null){
+            return combined;
+        }
+
+        combined.addAll(aMap.get(0));
+        return fanOut(aMap, combined,1);
+    }
+
+    private static List<String> fanOut(Map<Integer,List<String>> aMap, List<String> aCollection, Integer idx){
+        List<String> currentSeg = aMap.get(idx);
+        if(currentSeg==null){
+            return aCollection;
+        }
+        List<String> fanOutResult = new ArrayList<>();
+        aCollection.forEach(e-> currentSeg.forEach(seg-> fanOutResult.add(e + " " + seg)));
+        return fanOut(aMap,fanOutResult,++idx);
+    }
+
+    private static void print(String prefix, List<String> content){
+        content.forEach(e-> System.out.println("[" + prefix + "] " + e));
+
+    }
+
+}
+
---
+++ src/test/java/tool/MyTranslatorTest.java
@@ -0,0 +1,58 @@
+package tool;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+import static org.junit.Assert.*;
+
+
+public class MyTranslatorTest {
+
+    @Test
+    public void testPlainText(){
+        String s = "How are you?";
+        String expected = "How are you?";
+        List<String> result = MyTranslator.translate(s);
+        assertEquals(expected, print(result));
+    }
+
+    @Test
+    public void testSimpleOr(){
+        String s = "I know when John|Jim is coming|leaving";
+        String expected = "I know when John is coming; I know when John is leaving; I know when Jim is coming; I know when Jim is leaving";
+        List<String> result = MyTranslator.translate(s);
+        assertEquals(expected, print(result));
+    }
+
+    @Test
+    public void testOrWithBrackets(){
+        String s = "{{It is}|It's} up to you|him|her";
+        String expected = "It is up to you; It is up to him; It is up to her; It's up to you; It's up to him; It's up to her";
+        List<String> result = MyTranslator.translate(s);
+        assertEquals(expected, print(result));
+    }
+
+    @Test
+    public void testTwoParallelBrackets(){
+        String s = "{I'm good}|{It is fabulous|fantastic}";
+        String expected = "I'm good; It is fabulous; It is fantastic";
+        List<String> result = MyTranslator.translate(s);
+        assertEquals(expected, print(result));
+    }
+
+    @Test
+    public void testWithNestedBrackets(){
+        String s = "{Jim|Kate} helped {{Mr Huang}|{Ms Huang}} a lot {{removing snow}|{cutting grass}}";
+        String expected = "Jim helped Mr Huang a lot removing snow; Jim helped Mr Huang a lot cutting grass; Jim helped Ms Huang a lot removing snow; Jim helped Ms Huang a lot cutting grass; Kate helped Mr Huang a lot removing snow; Kate helped Mr Huang a lot cutting grass; Kate helped Ms Huang a lot removing snow; Kate helped Ms Huang a lot cutting grass";
+        List<String> result = MyTranslator.translate(s);
+        assertEquals(expected, print(result));
+    }
+
+    private String print(List<String> list){
+        StringJoiner sj = new StringJoiner("; ");
+        list.forEach(e->sj.add(e));
+        return sj.toString();
+    }
+}
